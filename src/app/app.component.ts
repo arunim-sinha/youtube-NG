5cdade023b1376a9ac2be59efc812c4623e673eb
Arunim Sinha <arunim.sinha@hotmail.com>
2025-01-31 11:48:02 +0530

    header adjust

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { FooterComponent } from "./shared/footer/footer.component";
-import { HeaderComponent } from "./shared/header/header.component";
+import { FooterComponent } from "./shared/components/footer/footer.component";
+import { HeaderComponent } from "./shared/components/header/header.component";
 import { RouterModule } from '@angular/router';
 
 @Component({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module'; // Import the main app component
 //import { AppRoutingModule } from './app-routing.module'; // Import routing module
 
