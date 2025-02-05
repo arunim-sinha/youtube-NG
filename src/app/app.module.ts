030253abda1a5f41b3e92c2daccf832706748349
Arunim Sinha <arunim.sinha@hotmail.com>
2025-02-05 17:42:29 +0530

    module test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
-import { ApplicationRef, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module'; // Import the main app component
-import { AppComponent } from './app.component';
 //import { AppRoutingModule } from './app-routing.module'; // Import routing module
 
 @NgModule({
