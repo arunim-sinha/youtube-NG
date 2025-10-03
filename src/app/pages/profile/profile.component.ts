67f572decf8508c831ff3db78eebc2b421ac38e0
Arunim Sinha <arunim.sinha@hotmail.com>
2025-10-03 08:04:45 +0530

    test

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,17 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
 import { LocalStorageUtil } from '@app/core/Utils/LocalStorage.util';
 
 @Component({
   selector: 'app-profile',
+  imports: [FormsModule, CommonModule , InputTextModule, PasswordModule, ButtonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
-  user: any = {};
+  user: any = {
+    name: '',
+    email: '',
+    profilePicture: '',
+    password: ''
+  };
+editEmail: any;
+editPassword: any;
+userForm: any;
   constructor( private storage: LocalStorageUtil) {
     const token = this.storage.getToken();
+    console.log('Token loaded from storage:', token);
     if (token) {
-      this.user = this.storage.getUser();
+      this.user.name = this.storage.getUser();
+      console.log('User data loaded from storage:', this.user);
     }
   }
 }
